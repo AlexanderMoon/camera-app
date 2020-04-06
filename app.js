8a06c0209c15e30457ef12702bae9eb51a4ba949
Alexander Moon <56555998+AlexanderMoon@users.noreply.github.com>
2020-04-06 20:44:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -29,22 +29,5 @@ cameraTrigger.onclick = function() {
     cameraOutput.classList.add("taken");
 };
 
-function gotDevices(mediaDevices) {
-  select.innerHTML = '';
-  select.appendChild(document.createElement('option'));
-  let count = 1;
-  mediaDevices.forEach(mediaDevice => {
-    if (mediaDevice.kind === 'videoinput') {
-      const option = document.createElement('option');
-      option.value = mediaDevice.deviceId;
-      const label = mediaDevice.label || `Camera ${count++}`;
-      const textNode = document.createTextNode(label);
-      option.appendChild(textNode);
-      select.appendChild(option);
-    }
-  });
-
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
-
-navigator.mediaDevices.enumerateDevices().then(gotDevices);
